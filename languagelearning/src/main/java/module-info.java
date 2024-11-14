7d391c6449a3bec890032d56d2662271102cb35f
KyleHix <khix@email.sc.edu>
2024-11-14 08:59:09 -0500

    test

--- languagelearning/src/main/java/module-info.java
+++ languagelearning/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.language {
     requires javafx.controls;
     requires javafx.fxml;
+    requires junit;
+    requires json.simple;
 
     opens com.language to javafx.fxml;
     exports com.language;
