8fbec436cf333a1f501041364b774e9267272369
KyleHix <khix@email.sc.edu>
2024-12-02 11:01:34 -0500

    updated dashboard

--- languagelearning/src/main/java/com/controllers/LoginController.java
+++ languagelearning/src/main/java/com/controllers/LoginController.java
@@ -35,7 +35,7 @@ public class LoginController {
             cmf.login(username, password);
             System.out.println("Successful Login!");
             // App.setRoot("dashboard");
-            App.setRoot("coursehome");
+            App.setRoot("dashboard");
         } else{
             Alert alert = new Alert(AlertType.INFORMATION);
             alert.setContentText("Incorrect Username or Password");
