989695bb23591f28a9369cf664d1166620587459
dw42CSCE <dw42@email.sc.edu>
2024-12-03 10:23:07 -0500

    argh

--- languagelearning/src/main/java/com/controllers/LessonController.java
+++ languagelearning/src/main/java/com/controllers/LessonController.java
@@ -2,23 +2,46 @@ package com.controllers;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
-import javafx.scene.control.TextField;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import com.model.*;
 
 import com.language.App;
-import com.model.CourseManagerFacade;
-import com.model.Exercise;
-import com.narration.*;
-import com.model.User;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 
 public class LessonController {
 
     private CourseManagerFacade cmf;
 
     @FXML
-    public void initialize(){
-        cmf = cmf.getInstance();
+    private Text lessonIntro;
+
+    @FXML
+    private Text lessonKeyWords;
+
+    @FXML
+    private Text lessonTitleText;
+
+    @FXML
+    private Rectangle lessonKeyWordsBg; 
+
+    @FXML
+    public void initialize() {
+        // Initialize the CourseManagerFacade instance
+        cmf = CourseManagerFacade.getInstance();
+        // Set the lesson data dynamically
+        updateLessonContent();
+    }
+
+    private void updateLessonContent() {
+        // Example lesson data fetching from CourseManagerFacade
+        String lessonTitle = cmf.getLesson().getSubject();
+        String lessonIntroduction = cmf.getLesson().getIntro();
+        String lessonKeywords = cmf.getLesson().toString();
+
+        // Update the text elements in the UI
+        lessonTitleText.setText(lessonTitle);
+        lessonIntro.setText(lessonIntroduction);
+        lessonKeyWords.setText("Keywords: " + lessonKeywords);
     }
 
     @FXML
@@ -32,19 +55,24 @@ public class LessonController {
     }
 
     @FXML
-    public void nextQuestion() throws IOException{
-        cmf.generateExercise(); 
+    public void nextQuestion() throws IOException {
+        cmf.generateExercise();
         Exercise nextExercise = cmf.getExercise();
-        if(nextExercise.getType().equals("translation")){
-            System.out.println("Switching to translation");
-            App.setRoot("translation");
-        } else if(nextExercise.getType().equals("fillin")){
-            System.out.println("Switching to fillin");
-            App.setRoot("fillin");
-        }  else if(nextExercise.getType().equals("audio")){
-            System.out.println("Switching to audio");
-            App.setRoot("audio");
+        switch (nextExercise.getType()) {
+            case "translation":
+                System.out.println("Switching to translation");
+                App.setRoot("translation");
+                break;
+            case "fillin":
+                System.out.println("Switching to fillin");
+                App.setRoot("fillin");
+                break;
+            case "audio":
+                System.out.println("Switching to audio");
+                App.setRoot("audio");
+                break;
+            default:
+                System.err.println("Unknown exercise type!");
         }
     }
-    
 }
