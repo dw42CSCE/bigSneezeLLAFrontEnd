26c16304529678fbce6edd55dc6c006bee7c2b02
KyleHix <khix@email.sc.edu>
2024-12-03 15:39:43 -0500

    lessonsummary controller

---
+++ languagelearning/src/main/java/com/controllers/LessonSummaryController.java
@@ -0,0 +1,28 @@
+package com.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+public class LessonSummaryController {
+
+    @FXML
+    private Text lessonTitleText;
+
+    @FXML
+    void nextQuestion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void switchToDashboard(MouseEvent event) {
+
+    }
+
+    @FXML
+    void switchToSettings(MouseEvent event) {
+
+    }
+
+}
