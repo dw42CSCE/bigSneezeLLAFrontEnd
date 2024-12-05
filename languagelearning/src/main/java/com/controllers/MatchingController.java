af556b73802c1ba7851062e7b001f2df0a5643b8
dw42CSCE <dw42@email.sc.edu>
2024-12-05 12:28:48 -0500

    argh

--- languagelearning/src/main/java/com/controllers/MatchingController.java
+++ languagelearning/src/main/java/com/controllers/MatchingController.java
@@ -99,7 +99,7 @@ public class MatchingController {
     void nextQuestion(ActionEvent event) throws IOException {
         if(correct){
             System.out.println("Current Score: " + cmf.getCurrentScore());
-            if(cmf.getCurrentScore() >=5 && cmf.getUser().getCourseProgress(cmf.getCourse()) <= cmf.getCourse().getLessons().indexOf(cmf.getLesson())){
+            if(cmf.getCurrentScore() >=5 && cmf.getUser().getCourseProgress(cmf.getCourse()) < cmf.getCourse().getLessons().size() - 1 && cmf.getUser().getCourseProgress(cmf.getCourse()) <= cmf.getCourse().getLessons().indexOf(cmf.getLesson())){
                 System.out.println("adding course progress");
                 cmf.addCourseProgress();
                 System.out.println("Course Progress: " + cmf.getUser().getCourseProgress(cmf.getCourse()));
