3b9321829fd8180e6f7df1690e36be5d6e23b0b2
logan-df <ldford711@gmail.com>
2024-12-04 22:52:08 -0500

    fillin controller

--- languagelearning/src/main/java/com/controllers/LessonFillinController.java
+++ languagelearning/src/main/java/com/controllers/LessonFillinController.java
@@ -1,11 +1,19 @@
 package com.controllers;
 
 import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.text.Text;
 
 import com.language.App;
+import com.model.Audio;
 import com.model.CourseManagerFacade;
+import com.model.Exercise;
+import com.model.FillIn;
+import com.model.Lesson;
 import com.narration.*;
 import com.model.User;
 import javafx.scene.control.Alert;
@@ -14,16 +22,120 @@ import javafx.scene.control.Alert.AlertType;
 public class LessonFillinController {
 
     private CourseManagerFacade cmf;
+    private Lesson currentLesson;
+    private FillIn currentExercise;
+    private boolean correct;
 
     @FXML
-    private void switchToDashboard() throws IOException {
-        App.setRoot("dashboard");
+    private Text lessonTitleText;
+
+    @FXML
+    private Text questionText;
+
+    @FXML
+    private TextField answerField;
+
+    public LessonFillinController() {
+        cmf = cmf.getInstance();
     }
+
+    @FXML
+    public void initialize() {
+        currentLesson = cmf.getLesson();
+        currentExercise = (FillIn) cmf.getExercise();
+
+        if (currentLesson != null && currentExercise != null) {
+            lessonTitleText.setText(currentLesson.getSubject());
+            questionText.setText(currentExercise.toString());
+        } else {
+            System.out.println("Lesson or exercise is null.");
+        }
+    }
+
+    @FXML
+    public void checkAnswers(ActionEvent event) {
+        String selectedAnswer = answerField.getText();
+        if (currentExercise.isCorrect(selectedAnswer)) {
+            System.out.println("Correct Answer!");
+            correct = true;
+            if(currentExercise.getFirstTry()){
+                cmf.incrementScore();
+                System.out.println("Adding to score: " + cmf.getCurrentScore());
+            }
+        } else {
+            cmf.addWord(currentExercise.getWord());
+            currentExercise.tried();
+            System.out.println("Incorrect Answer!");
+        }
+    }
+
+    @FXML
+    void nextQuestion(ActionEvent event) throws IOException {
+        if(correct){
+            System.out.println("Current Score: " + cmf.getCurrentScore());
+            if(cmf.getCurrentScore() >=5 && cmf.getUser().getCourseProgress(cmf.getCourse()) <= cmf.getCourse().getLessons().indexOf(cmf.getLesson())){
+                System.out.println("adding course progress");
+                cmf.addCourseProgress();
+                System.out.println("Course Progress: " + cmf.getUser().getCourseProgress(cmf.getCourse()));
+            }
+            System.out.println(cmf.getLessonProgress());
+            cmf.incrementLessonProgress();
+            if(cmf.getLessonProgress() >= 5){
+                System.out.println("Switching to summary");
+                App.setRoot("lessonsummary");
+            } else{
+                cmf.generateExercise(); 
+                Exercise nextExercise = cmf.getExercise();
+                if(nextExercise.getType().equals("translation")){
+                    System.out.println("Switching to translation");
+                    App.setRoot("translation");
+                } else if(nextExercise.getType().equals("fillin")){
+                    System.out.println("Switching to fillin");
+                    App.setRoot("fillin");
+                }  else if(nextExercise.getType().equals("audio")){
+                    System.out.println("Switching to audio");
+                    App.setRoot("audio");
+                }
+            }
+        }
+    }
+
+    @FXML
+    void previousQuestion(ActionEvent event) throws IOException {
+        if (cmf.getLessonProgress() > 1) {
+            cmf.decrementLessonProgress();
+            Exercise prevExercise = cmf.getExercises().get(cmf.getLessonProgress() - 1);
+            initialize();
     
-
-    @FXML
-    private void switchToSettings() throws IOException {
-        App.setRoot("settings");
+            switch (prevExercise.getType()) {
+                case "translation":
+                    System.out.println("Switching to translation");
+                    App.setRoot("translation");
+                    break;
+                case "fillin":
+                    System.out.println("Switching to fillin");
+                    App.setRoot("fillin");
+                    break;
+                case "audio":
+                    System.out.println("Switching to audio");
+                    App.setRoot("audio");
+                    break;
+                default:
+                    System.out.println("Unknown exercise type");
+                    break;
+            }
+        } else {
+            System.out.println("Already at the first question. Cannot go back.");
+        }
     }
 
+    @FXML
+   private void switchToDashboard() throws IOException {
+      App.setRoot("dashboard");
+   }
+
+   @FXML
+   private void switchToSettings() throws IOException {
+      App.setRoot("settings");
+   }
 }
